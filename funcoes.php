744658f874b04511b223cf659eaef902b4768281
unknown <maluribeiro08@gmail.com>
2021-09-07 14:03:44 -0300

    Adicionando funcao para cadrastro funcionario - ainda não funcionando

--- funcoes.php
+++ funcoes.php
@@ -25,6 +25,7 @@ function contarFuncionariosTotal($listaFuncionarios)
 function buscarFuncionario($listaFuncionarios, $palavraChavePesquisa)
 {
     $funcionariosFiltro = [];
+
     foreach($listaFuncionarios as $funcionario) //procura na lista toda
     {
         if(mb_strpos(strtoupper($funcionario->first_name), strtoupper($palavraChavePesquisa)) !== false)//se achar
@@ -43,13 +44,43 @@ function buscarFuncionario($listaFuncionarios, $palavraChavePesquisa)
         {
             $funcionariosFiltro [] = $funcionario;
         }
-
-        /*if($funcionario->first_name == $palavraChavePesquisa) //se achar
-         {
-            $funcionariosFiltro [] = $funcionario;// guarda o nome na lista de encontrados
-        }*/
     }
+
     return $funcionariosFiltro;//ao fim da pesquisa, retorna todos os alunos encontrados
 }
 
+function registrarFuncionario ($arquivoJson, $idFuncionario, $firstName, $lastName, $email, $gender, $ipAddress, $country, $department)
+{
+    //pega o arquivo JSON e transforma em array
+        $jsonArray = json_decode($arquivoJson);
+    
+    //adiciona na ultima posição do array o novo funcionário
+        //$funcionario = 
+        $jsonArray [0] =
+        [
 
+            "id" => $idFuncionario,
+            "first_name" => $firstName,
+            "last_name" => $lastName,
+            "email" => $email,
+            "gender" => $gender,
+            "ip_address" => $ipAddress,
+            "country" => $country,
+            "department" => $department
+        ];
+
+        //$jsonArray[0]=$funcionario;
+
+    //transforma o array em JSON
+        //$funcionarioJson = json_encode($jsonArray);
+        $jsonFinal = json_encode($jsonArray);
+
+    //abre o arquivo JSON para edição
+        $arquivoEditar = fopen($arquivoJson, "a");
+
+    //escreve no arquivo json
+        fwrite($arquivoEditar, $jsonFinal);
+
+    //fecha o arquivo
+        fclose($arquivoEditar);
+}
